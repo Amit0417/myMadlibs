011bd9b1fdea0b0db64889079e1e14d67cbbb608
AMIT KUMAR MAHOUR <r.amit.mahour@gmail.com>
2021-09-12 19:55:43 +0530

    update

---
+++ mYmaDlib.py
@@ -0,0 +1,6 @@
+import random
+from rMad import random1, random2, random3, random4, random5, random6
+
+if __name__ == '__main__':
+    my = random.choice([random1, random2, random3, random4, random5, random6])
+    my.madlib()
